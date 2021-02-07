e8b2874395864f33a670369f69caa30eeda90944
het <hetrpatel1506@gmail.com>
2021-02-07 12:54:12 +0530

    Form validation complete

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -75,34 +75,11 @@ class BurgerBuilder extends Component {
         this.setState({purchasing: false})
     }
     purchaseContinueHandler = () => {
-        // this.setState({loading: true})
-        // const order = {
-        //     ingredients: this.state.ingredients,
-        //     price: this.state.totalPrice,
-        //     customer: {
-        //         name: 'Het Patel',
-        //         address: {
-        //             city: 'ahemedabad',
-        //             state: 'gujrat',
-        //             country: 'india'
-        //         },
-        //         email: 'hello@hello.com',
-        //     },
-        //     deliveryMethod: 'fastest'
-        // }
-        
-        // axios.post('/orders.json', order)
-        //     .then((response) => {
-        //         this.setState({loading: false, purchasing: false})
-        //     })
-        //     .catch((error) => {
-        //         // this.setState({loading: false, purchasing: false})
-        //         this.setState({isError: true})
-        //     })
         const queryParams = [];
         for(let i in this.state.ingredients){
             queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
         }
+        queryParams.push('price=' + this.state.totalPrice);
         const queryString = queryParams.join('&');
         this.props.history.push({
             pathname: '/checkout',
